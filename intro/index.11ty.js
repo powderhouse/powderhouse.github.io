87c7544b9846beeea849f7466763a1cd3252e8fb
Alec Resnick <aresnickety@gmail.com>
2021-01-25 16:46:33 -0500

    Converting loop to filter + map for intro

--- intro/index.11ty.js
+++ intro/index.11ty.js
@@ -7,7 +7,15 @@ exports.data = {
 exports.render = function(data) {
     return `
     <div id='intro-container'>
-        <img id='stop-work' class='w-full' alt="${data.content.intro.review_header.alt}" src="${data.content.intro.review_header.url}"/>
+        <div id="letter-image">
+            <img id='stop-work' class='w-full' alt="${data.content.intro.review_header.alt}" src="${data.content.intro.review_header.url}"/>
+        </div>
+        <div id="letter-date">
+            ${data.content.intro.letter_date}
+        </div>
+        <div id="letter-body">
+            ${data.content.intro.letter_content.filter(c => c.type == 'paragraph').map(c => `<p>${c.text}</p>`).join('\n')}
+        </div>
     </div>
     `;
 };
