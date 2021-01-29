d126414c2bff9798f4fe821c795bc6d777966384
Alec Resnick <aresnickety@gmail.com>
2021-01-28 22:05:23 -0500

    Remove printing from data loading

--- _data/content.js
+++ _data/content.js
@@ -3,21 +3,19 @@ var Prismic = require('prismic-javascript');
 var apiEndpoint = "https://powderhs.cdn.prismic.io/api/v2";
 
 module.exports = async function() {
-    return await Prismic.getApi(apiEndpoint).then(async function(api) {
-        var getByID = async function(id) {
-            return await api.getByID(id).then(function(document) {
-                var data = document.data;
-                console.log("Getting", id, "got", data);
-                return data;
-            });
-        };
+ return await Prismic.getApi(apiEndpoint).then(async function(api) {
+     var getByID = async function(id) {
+         return await api.getByID(id).then(function(document) {
+             var data = document.data;
+             return data;
+         });
+     };
 
-        [splash, intro] = await Promise.all(['X-v1WBEAACMAIOkL','X-v3UBEAACAAIPHA'].map(id => getByID(id))).then(values => values);
+     [splash, intro] = await Promise.all(['X-v1WBEAACMAIOkL','X-v3UBEAACAAIPHA'].map(id => getByID(id))).then(values => values);
 
-        console.log("Now splash and intro are", splash, intro)
-        return {
-            splash: splash,
-            intro: intro
-        }
-    });
+     return {
+         splash: splash,
+         intro: intro
+     }
+ });
 };
