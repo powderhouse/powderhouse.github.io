3a8a366527198580d664f2415c2518205ee0ae42
Alec Resnick <aresnickety@gmail.com>
2021-01-28 22:07:58 -0500

    Modify intro to use PrismicDOM

--- intro/index.11ty.js
+++ intro/index.11ty.js
@@ -1,21 +1,58 @@
-// /intro
+// intro
+
+// Bring in PrismicDOM helper for rendering HTML
+import PrismicDOM from 'prismic-dom';
+const html = PrismicDOM.RichText.asHtml
 
 exports.data = {
     layout: "layouts/default.11ty.js"
 }
 
 exports.render = function(data) {
-    return `
-    <div id='intro-container'>
-        <div id="letter-image">
-            <img id='stop-work' class='w-full' alt="${data.content.intro.review_header.alt}" src="${data.content.intro.review_header.url}"/>
-        </div>
-        <div id="letter-date">
-            ${data.content.intro.letter_date}
-        </div>
-        <div id="letter-body">
-            ${data.content.intro.letter_content.filter(c => c.type == 'paragraph').map(c => `<p>${c.text}</p>`).join('\n')}
-        </div>
-    </div>
+    const intro = data.content.intro;
+    console.log(intro.body[0].items[0]);
+
+    let header = `
+    <div id='stop-work-header'>
+     <img id='stop-work' class='w-full' alt="${intro.review_header.alt}" src="${intro.review_header.url}"/>
+    </div>   
     `;
-};
+
+    let letter = `
+    <div id='letter-content'>
+        ${html(intro.letter_content)}
+     </div>
+    `
+
+    let closing = `
+    <div id='letter-closing'>
+         <div id='signatures'>
+            ${
+                intro.body[0].items.map(i => `
+                <div class='signature-image'>
+                    <img src="${i.signature_image.url}"/>
+                </div>
+                <div class='signature-person'>
+                    ${html(i.printed_name)}
+                </div>       
+                    `).join('\n')
+            }
+         </div>
+     </div>
+    `;
+
+    let toc = `
+    <div id='table-of-contents'>
+        <div class='toc-item'></div>
+    </div>
+    `
+
+return `
+ <section id='intro'>
+    ${header}
+    ${letter}
+    ${closing}
+    ${toc}
+ </section>
+`
+}
