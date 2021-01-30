ab363e2f1ec954a73b06f4a6bfae62d470b7960e
Alec Resnick <aresnickety@gmail.com>
2021-01-30 10:25:50 -0500

    First commit of meditations section

---
+++ meditations/index.11ty.js
@@ -0,0 +1,10 @@
+import PrismicDOM from 'prismic-dom';
+const html = PrismicDOM.RichText.asHtml
+
+exports.data = {
+    layout: "layouts/default.11ty.js"
+}
+
+exports.render = function(data) { 
+
+}
