9960a75ddd642c0f811aae2d5c8424da5f776d5a
Alec Resnick <aresnickety@gmail.com>
2021-01-30 13:29:11 -0500

    Refactoring data import from Prismic to easily add pages

--- _data/content.js
+++ _data/content.js
@@ -3,19 +3,32 @@ var Prismic = require('prismic-javascript');
 var apiEndpoint = "https://powderhs.cdn.prismic.io/api/v2";
 
 module.exports = async function() {
- return await Prismic.getApi(apiEndpoint).then(async function(api) {
-     var getByID = async function(id) {
-         return await api.getByID(id).then(function(document) {
-             var data = document.data;
-             return data;
-         });
-     };
+    return await Prismic.getApi(apiEndpoint).then(async function(api) {
+        let getByID = async function(id) {
+            let doc = await api.getByID(id).then(d => d);
+            return doc.data;
+        };
 
-     [splash, intro] = await Promise.all(['X-v1WBEAACMAIOkL','X-v3UBEAACAAIPHA'].map(id => getByID(id))).then(values => values);
+        let pageIDs = {
+            splash: 'X-v1WBEAACMAIOkL',
+            intro: 'X-v3UBEAACAAIPHA',
+            meditations: 'X-vzrBEAACIAIOGo',
+            // sights: 'X-vUJxEAACAAIFaw',
+            // sounds: 'X-vvvxEAACIAINBa',
+            // circles: 'X-vZzxEAACAAIG-9',
+            // administrivia: 'X-v7WREAACEAIQOX',
+            // prototypes: 'X-v9ThEAACMAIQw8',
+            // inquiry: 'X-wOIBEAACEAIVa1',
+            // algebra: 'X-wqHREAACIAIdJN',
+            // random: 'X-wgFxEAACIAIaX6',
+            // moving: 'X-wYOBEAACEAIYM2',
+            // outro: 'X-wn_xEAACIAIcjx'
+        };
 
-     return {
-         splash: splash,
-         intro: intro
-     }
- });
+        for ([page, id] of Object.entries(pageIDs)) {
+            pageIDs[page] = await getByID(id)
+        }
+
+        return pageIDs;
+    });
 };
