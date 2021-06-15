d78980365c095e3abbb1281050fb26be8b374e4e
Alec Resnick <aresnickety@gmail.com>
2021-06-14 20:40:38 -0400

    Adding pass through media include

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
-module.exports = {
-    dir: {
-        // ⚠️ These values are both relative to the eleventy input directory.
-        includes: "_includes",
-        layouts: "_layouts"
-    }
+module.exports = function (eleventyConfig) {
+    // via https://www.11ty.dev/docs/copy/#manual-passthrough-file-copy-(faster)
+    eleventyConfig.addPassthroughCopy("media");
+
+    return {
+        dir: {
+            // ⚠️ These values are both relative to the eleventy input directory.
+            includes: "_includes",
+            layouts: "_layouts",
+        },
+    };
 };
