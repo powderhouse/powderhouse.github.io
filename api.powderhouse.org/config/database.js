c3016b37311993c0c32fc77fbed6a12131ed4b42
Alec Resnick <alec@powderhouse.org>
2021-12-06 12:37:02 -0500

    Shifting sqlite DB out of tmp for versioning

--- api.powderhouse.org/config/database.js
+++ api.powderhouse.org/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'database/data.db')),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'data.db')),
     },
     useNullAsDefault: true,
   },
