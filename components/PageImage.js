27e64d4c7326ba589fbd336e98955bcbf86d6c58
Alec Resnick <alec@powderhouse.org>
2022-01-27 15:37:47 -0500

    Initial commit of PageImage component for handling baseline compatible resizing of image containers

---
+++ components/PageImage.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { useEffect, useState, useRef } from "react";
+
+let Resize = styled.div`
+	background-color: blue;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+`;
+
+function PageImage(props) {
+	// Adding `refs` to access DOM elements for our image and resizing div
+	// via https://reactjs.org/docs/hooks-reference.html#useref
+	const img = useRef(null);
+	const resizer = useRef(null);
+
+	let rootFontSizeInPx, rootLineHeightInPx, rootLineHeightInRem;
+	useEffect(() => {
+		// When the component loads, create an observer
+		let observer = new ResizeObserver((entries) => {
+			// via https://web.dev/resize-observer/
+			for (let entry of entries) {
+				const rectangle = entry.contentRect;
+
+				// Count the number of lines and set our height
+				let numLines = Math.ceil(rectangle.height / rootLineHeightInPx);
+				let calcString = `calc(${numLines} * ${rootLineHeightInRem}rem)`;
+
+				resizer.current.style.height = calcString;
+			}
+		});
+
+		// Calculate line height for use in computing number of lines
+		let bodyStyle = window.getComputedStyle(document.body);
+		rootFontSizeInPx = parseInt(bodyStyle.fontSize);
+		rootLineHeightInPx = parseInt(bodyStyle.lineHeight);
+		rootLineHeightInRem = rootLineHeightInPx / rootFontSizeInPx;
+
+		// Attach the observer to watch our image
+		observer.observe(img.current);
+	});
+
+	let container = (
+		<div className={props.fullBleed ? "full-bleed" : ""}>
+			<Resize ref={resizer}>
+				<img ref={img} src={props.src} />
+			</Resize>
+		</div>
+	);
+	return container;
+}
+
+export default PageImage;
