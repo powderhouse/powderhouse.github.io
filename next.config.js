985edff4f653ba8d48b12aa18a5fc3c571f8e9ff
Alec Resnick <alec@powderhouse.org>
2022-02-12 12:53:54 -0500

    Updating allowed image domains in anticipation of Image

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+// module.exports = withBundleAnalyzer({});
+
+module.exports = {
+  images: {
+    domains: ["powderhouse-strapi-uploads.s3.amazonaws.com"],
+  },
+};
