f7e19a9174ee7186cab36df4bd57996e38a1e3a7
Alec Resnick <alec@powderhouse.org>
2022-02-14 17:01:20 -0500

    Beginning to add media queries to RegionContainer

--- components/RegionContainer2.js
+++ components/RegionContainer2.js
@@ -1,6 +1,51 @@
 import styled from "styled-components";
 import { colorByProp } from "../components/global.js";
 
+const BREAKPOINTS = {
+	mobileMax: 550,
+	tabletMax: 1100,
+	laptopMax: 1440,
+};
+const mediaQueries = {
+	uptoMobile: `(max-width: ${BREAKPOINTS.mobileMax}px)`,
+	uptoTablet: `(max-width: ${BREAKPOINTS.tabletMax}px)`,
+	uptoLaptop: `(max-width: ${BREAKPOINTS.laptopMax}px)`,
+};
+
+let mediaQueryWrap = function (mq) {
+	return (style) => `@media ${mediaQueries[mq]} { ${style} }`;
+};
+
+let paddingByMedia = {
+	top: {
+		uptoMobile: `calc(4 * 1.3rem)`,
+		uptoTablet: `calc(4 * 1.3rem)`,
+		uptoLaptop: `calc(4 * 1.3rem)`,
+	},
+	bottom: {
+		uptoMobile: `calc(4 * 1.3rem)`,
+		uptoTablet: `calc(4 * 1.3rem)`,
+		uptoLaptop: `calc(4 * 1.3rem)`,
+	},
+};
+
+// let RegionContainer = styled("div")`
+// 	position: relative;
+// 	${(props) => colorByProp(props)}
+
+// 	${(props) =>
+// 		["top", "bottom"].map((direction) =>
+// 			props.pad.includes(direction)
+// 				? Object.keys(mediaQueries).map(
+// 						(mq) =>
+// 							mediaQueryWrap(
+// 								mq
+// 							)`padding-${direction}: ${paddingByMedia[direction][mq]}`
+// 				  )
+// 				: `initial`
+// 		)}
+// `;
+
 let RegionContainer = styled("div")`
 	position: relative;
 	${(props) => colorByProp(props)}
