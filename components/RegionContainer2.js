97de3e0a7ee08ef7e98faec1bae70e2603042703
Alec Resnick <alec@powderhouse.org>
2022-02-14 21:06:56 -0500

    Commit of media query harness for modifying padding in Regions— There must be a simpler way.

--- components/RegionContainer2.js
+++ components/RegionContainer2.js
@@ -5,63 +5,116 @@ const BREAKPOINTS = {
 	mobileMax: 550,
 	tabletMax: 1100,
 	laptopMax: 1440,
+	minLaptop: 1440.1,
 };
+
 const mediaQueries = {
 	uptoMobile: `(max-width: ${BREAKPOINTS.mobileMax}px)`,
 	uptoTablet: `(max-width: ${BREAKPOINTS.tabletMax}px)`,
 	uptoLaptop: `(max-width: ${BREAKPOINTS.laptopMax}px)`,
+	minLaptop: `(min-width: ${BREAKPOINTS.minLaptop}px)`,
 };
 
-let mediaQueryWrap = function (mq) {
-	return (style) => `@media ${mediaQueries[mq]} { ${style} }`;
-};
-
-let paddingByMedia = {
-	top: {
-		uptoMobile: `calc(4 * 1.3rem)`,
-		uptoTablet: `calc(4 * 1.3rem)`,
-		uptoLaptop: `calc(4 * 1.3rem)`,
+let stylesByMedia = {
+	// A dictionary of styles to apply per media query
+	uptoMobile: {
+		"padding-top": `calc(4 * 1.3rem)`,
+		"padding-bottom": `calc(4 * 1.3rem)`,
 	},
-	bottom: {
-		uptoMobile: `calc(4 * 1.3rem)`,
-		uptoTablet: `calc(4 * 1.3rem)`,
-		uptoLaptop: `calc(4 * 1.3rem)`,
+	uptoTablet: {
+		"padding-top": `calc(4 * 1.3rem)`,
+		"padding-bottom": `calc(4 * 1.3rem)`,
+	},
+	uptoLaptop: {
+		"padding-top": `calc(4 * 1.3rem)`,
+		"padding-bottom": `calc(4 * 1.3rem)`,
+	},
+	minLaptop: {
+		"padding-top": `calc(4 * 1.3rem)`,
+		"padding-bottom": `calc(4 * 1.3rem)`,
 	},
 };
 
-// let RegionContainer = styled("div")`
-// 	position: relative;
-// 	${(props) => colorByProp(props)}
+let mq = function (media) {
+	// A function which wraps its argument in the right media query
+	return (style) => `@media ${mediaQueries[media]} {\n${style}\n}`;
+};
 
-// 	${(props) =>
-// 		["top", "bottom"].map((direction) =>
-// 			props.pad.includes(direction)
-// 				? Object.keys(mediaQueries).map(
-// 						(mq) =>
-// 							mediaQueryWrap(
-// 								mq
-// 							)`padding-${direction}: ${paddingByMedia[direction][mq]}`
-// 				  )
-// 				: `initial`
-// 		)}
-// `;
+let getPadding = (media, props) => {
+	console.log("Looking at", media, "and", props);
+	let paddingString = ["top", "bottom"]
+		.map(
+			// Create padding-top and bottom strings
+			(direction) =>
+				`padding-${direction}: ${
+					props.pad && props.pad.includes(direction)
+						? stylesByMedia[media][`padding-${direction}`]
+						: "initial"
+				}` + ";"
+		)
+		.join("\n");
+	return paddingString;
+};
+
+let getMediaQueryStyles = (props) => {
+	// This constructs the media queries for the paddings for regions.  Note that we only handle padding here.
+
+	// Grab the media types to iterate over
+	let mediaTypes = Object.keys(mediaQueries);
+	let mediaQueryWrappedPadding = mediaTypes
+		.map((media) => {
+			// For each media type, construct styles
+			let styles = [
+				getPadding(media, props), // Right now, we only have padding, but you can add strings to this array to have wrapped in media queries as appropriate
+			].join("\n");
+			return mq(media)(styles); // and return those padding-top and bottom strings wrapped in the appropriate media query
+		})
+		.join("\n");
+
+	// At this point, mediaQueryWrappedPadding looks something like
+	// 	@media (max-width: 550px) {
+	// padding-top: …;
+	// padding-bottom: …;
+	// }
+	// @media (max-width: 1100px) {
+	// padding-top: …;
+	// padding-bottom: …;
+	// }
+	// @media (max-width: 1440px) {
+	// padding-top: …;
+	// padding-bottom: …;
+	// }
+	// @media (min-width: 1440.1px) {
+	// padding-top: …;
+	// padding-bottom: …;
+	// }
+
+	return mediaQueryWrappedPadding;
+};
 
 let RegionContainer = styled("div")`
 	position: relative;
-	${(props) => colorByProp(props)}
 
-	padding-top: ${(props) =>
-		props.pad
-			? props.pad.includes("top")
-				? "calc(4 * 1.3rem)"
-				: "initial"
-			: "initial"};
-	padding-bottom: ${(props) =>
-		props.pad
-			? props.pad.includes("bottom")
-				? "calc(4 * 1.3rem)"
-				: "initial"
-			: "initial"};
+	${(props) => colorByProp(props)}
+	${(props) => getMediaQueryStyles(props)}
 `;
+
+// Older, non-media query version— Keeping until we finalize (14 February 2022)
+// let RegionContainer = styled("div")
+// 	position: relative;
+// 	${(props) => colorByProp(props)}
+// 	padding-top: ${(props) =>
+// 		props.pad
+// 			? props.pad.includes("top")
+// 				? "calc(4 * 1.3rem)"
+// 				: "initial"
+// 			: "initial"};
+// 	padding-bottom: ${(props) =>
+// 		props.pad
+// 			? props.pad.includes("bottom")
+// 				? "calc(4 * 1.3rem)"
+// 				: "initial"
+// 			: "initial"};
+// `;
 
 export default RegionContainer;
