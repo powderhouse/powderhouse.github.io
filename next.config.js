d9c95d2c15c9f04a31fa8f332c0b819e9a321720
Alec Resnick <alec@powderhouse.org>
2022-03-03 09:34:45 -0500

    Working on rewrites for archives

--- next.config.js
+++ next.config.js
@@ -11,6 +11,30 @@ let nextConfig = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  // webpack: (config, { dev, isServer }) => {
+  //   // Replace React with Preact only in client production build
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat",
+  //     });
+  //   }
+
+  //   return config;
+  // },
+  rewrites: async () => {
+    return [
+      // {
+      //   source: `/work/powderhouse-studios-archive/:path/:last(.+(?!\..+))`,
+      //   destination: "/powderhouse-studios-archive/:path/:last/index.html",
+      // },
+      {
+        source: "/work/powderhouse-studios-archive/:path*/:last",
+        destination: "/powderhouse-studios-archive/:path*/:last",
+      },
+    ];
+  },
 };
 
 let plugins = [
