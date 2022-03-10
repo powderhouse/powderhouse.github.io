a5dac13639ef13f13e7efae3f5189310cf06c0cd
Alec Resnick <alec@powderhouse.org>
2022-03-10 13:50:59 -0500

    Bringing in validationPaths, reformatting media queries

--- site-data.js
+++ site-data.js
@@ -25,29 +25,46 @@ let socials = [
 ];
 socials.forEach((s) => (s.id = s.service.toLowerCase() + "-" + "social"));
 
-let asteriskSVG = (strokeWidth = "1") => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-34 -39 150 111">
-    <path
-      vectorEffect="non-scaling-stroke"
-      d="M 0 43.712 L 88.25 26.838 M 57.055 78.651 L 27.423 0 M 23.298 65.652 L 57.156 8.351 M 67.46 48.363 L 7.632 19.816"
-      fill="transparent"
-      strokeWidth={strokeWidth}
-      strokeLinecap="square"
-      strokeLinejoin="round"
-    ></path>
-  </svg>
-);
-
-const BREAKPOINTS = {
+const breakpoints = {
+  mobileMin: 375,
   mobileMax: 550,
   tabletMax: 1100,
   laptopMax: 1440,
+  desktopMin: 1440 + 1,
 };
+
+const validationPaths = [
+  // TODO: Hardcoded for now, but should talk to CMS or similar
+  "/",
+  "/about",
+  "/team",
+  "/team/jobs",
+  "/team/jobs/financial-researcher",
+  "/team/jobs/legal-researcher",
+  "/work",
+  "/work/prospect-studio",
+  "/work/innovation-school",
+  "/work/equitable-enrollment",
+  "/work/creative-computing",
+  "/work/atlas",
+  "/work/digital-storytelling",
+  "/work/turtle-geometry",
+  "/work/dlcs-standards",
+  "/work/fabville",
+  "/work/healey-steam",
+  "/work/spaghetti-dinners",
+  "/work/signs-of-life",
+  "/work/flutes-and-waves",
+  "/work/bring-your-grandma-to-math",
+  "/work/nervous-nature",
+  "/work/streetbeest",
+  "/news",
+];
 
 const mediaQueries = {
-  uptoMobile: `(max-width: ${BREAKPOINTS.mobileMax}px)`,
-  uptoTablet: `(max-width: ${BREAKPOINTS.tabletMax}px)`,
-  uptoLaptop: `(max-width: ${BREAKPOINTS.laptopMax}px)`,
+  uptoMobile: `(max-width: ${breakpoints.mobileMax}px)`,
+  uptoTablet: `(max-width: ${breakpoints.tabletMax}px)`,
+  uptoLaptop: `(max-width: ${breakpoints.laptopMax}px)`,
 };
 
-export { navMenuItems, socials, asteriskSVG, mediaQueries };
+export { navMenuItems, socials, mediaQueries, breakpoints, validationPaths };
