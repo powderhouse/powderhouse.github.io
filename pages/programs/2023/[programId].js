610fe6842e25d94ab379c813cd2e3e7a492eb5d7
Shaunalynn Duffy <shduff@Shaunalynns-MBP.hsd1.ma.comcast.net>
2023-06-21 16:40:03 -0400

    removing findLargestFormat import to solve Vercel deploy error

--- pages/programs/2023/[programId].js
+++ pages/programs/2023/[programId].js
@@ -14,7 +14,7 @@ import {
   PageSectionContent,
   PageHeading,
 } from "../../../components/Page.js";
-import { Div, findLargestFormat, slugify } from "../../../components/global";
+import { Div, slugify } from "../../../components/global";
 
 import { fetchAPI } from "../../../lib/api";
 import { useRouter } from "next/router";
